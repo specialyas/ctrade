848a42893d9e5c61a0eae3bf5711e81774b0195b
Aka'aba Musa Akidi <onlinemraam@gmail.com>
2023-01-23 10:10:56 +0100

    to replace tradename and logos

--- script.js
+++ script.js
@@ -5,19 +5,6 @@ function _(x) {
 let navbarCollapse = _("toggle-nav");
 let secondNav = _("second-nav");
 
-navbarCollapse.addEventListener("click", function () {
-  secondNav.classList.toggle("show-nav");
-});
-
-window.onresize = function () {
-  if (window.innerWidth > 820) {
-    // remove the second nav class
-    secondNav.classList.remove("show-nav");
-  }
-};
-
-// SLIDE SHOW
-
 let shows = [0, 1];
 
 let slidePrev = _("slide-prev");
@@ -28,43 +15,71 @@ let k = shows[shows.length - 1];
 
 let slideBox = document.getElementsByClassName("review-card");
 
+navbarCollapse.addEventListener("click", function () {
+  secondNav.classList.toggle("show-nav");
+});
+
+window.onresize = function () {
+  if (window.innerWidth > 820) {
+    // remove the second nav class
+    secondNav.classList.remove("show-nav");
+  }
+
+  if (window.innerWidth < 720) {
+    // display only one div
+    for (let i = 0; i < slideBox.length; i++) {
+      const element = slideBox[i];
+
+      if (!shows.includes(i)) element.style.display = "none";
+    }
+  }
+};
+
+// SLIDE SHOW
+
+for (let i = 0; i < slideBox.length; i++) {
+  const element = slideBox[i];
+
+  if (!shows.includes(i)) element.style.display = "none";
+}
+
 function disableSlides() {
   for (let i = 0; i < slideBox.length; i++) {
     const element = slideBox[i];
-
-    if (!shows.includes(i)) element.style.display = "none";
+    element.style.display = "none";
   }
 }
 
-disableSlides();
-
 slideNext.addEventListener("click", () => {
-  shows[0] = k;
-  k = k + 1;
-  if (k < slideBox.length) {
-    disableSlides();
-    shows.pop(k);
-    shows.push(k);
-    showSlide(shows);
+  // check the last array if not greater than length of the box
+
+  if (shows[0] + 2 < slideBox.length) {
+    shows[0] = shows[0] + 2;
   }
+
+  if (shows[1] + 2 < slideBox.length) {
+    shows[1] = shows[1] + 2;
+  }
+
+  disableSlides();
+  showSlide(shows);
 });
 
 slidePrev.addEventListener("click", () => {
   // check if the value is already 0
 
-  if (shows[0] != 0) {
+  if (shows[0] > 0) {
+    shows[0] = shows[0] - 2;
+    shows[1] = shows[1] - 2;
     disableSlides();
-    shows.pop(k);
-    shows.push(shows[0]);
-    shows[0] = shows[0] - 1;
+
+    showSlide(shows);
   }
-  showSlide(shows);
 });
 
 function showSlide(shows) {
-  console.log(shows);
   shows.forEach((box) => {
-    if (box < slideBox.length - 1 && box >= 0)
+    if (box < slideBox.length && box >= 0)
       slideBox[box].style.display = "block";
   });
 }
